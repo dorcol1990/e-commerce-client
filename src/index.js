5f9eee6ce46e22b2d14eaf2397aa09cc42c0fdb3
dorcol1990 <tickets1312@yahoo.com>
2023-09-26 12:50:09 +0200

    add redux file

--- src/index.js
+++ src/index.js
@@ -2,18 +2,27 @@ import "./index.css";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import Main from "./Main";
+import { Provider } from 'react-redux';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
+import cartReducer from './state';
+import { configureStore } from "@reduxjs/toolkit";
 import { theme } from "./theme";
+
+const store = configureStore({
+    reducer: {cart: cartReducer}
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Main />
-    </ThemeProvider>    
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Main />
+        </ThemeProvider>
+    </Provider>    
   </React.StrictMode>
 );
 
